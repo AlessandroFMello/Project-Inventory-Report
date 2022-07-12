287cf8f134964fdfa7e4fcba859e3e8f5862a98b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-07-12 16:09:48 -0300

    feat: Creates classmethod import_data with a report_generator and csv_reader

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,30 @@
+from csv import reader
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    def csv_reader(path):
+        with open(path, 'r', encoding='utf-8') as file:
+            csv_reader = reader(file, delimiter=',', quotechar='"')
+
+            head, *data = csv_reader
+
+        return [dict(zip(head, row)) for row in data]
+
+    def report_generator(data, type):
+        if type == 'simples':
+            return SimpleReport.generate(data)
+        elif type == 'completo':
+            return CompleteReport.generate(data)
+        else:
+            raise ValueError('Unknown type')
+
+    @classmethod
+    def import_data(cls, path, type):
+        if path.endswith("csv"):
+            data_file = cls.csv_reader(path)
+        else:
+            raise ValueError('Unable to read selected file')
+
+        return cls.report_generator(data_file, type)
